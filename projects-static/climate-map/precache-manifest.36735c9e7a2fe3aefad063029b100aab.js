1c8cd718b0cb4501e9f2ac138b50ec7a336e1d0a
Logan Williams <logan.williams@alum.mit.edu>
2019-06-06 08:10:30 -0700

    match b8787fb: Add updated climate map with ZIP code images

---
+++ projects-static/climate-map/precache-manifest.36735c9e7a2fe3aefad063029b100aab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "470cfdac598d82aba46fffb062eb5969",
+    "url": "/projects-static/climate-map/index.html"
+  },
+  {
+    "revision": "6a876f90c82bf32b3b1c",
+    "url": "/projects-static/climate-map/static/css/2.2614ebd1.chunk.css"
+  },
+  {
+    "revision": "ee34d0f2d1d06542f3f8",
+    "url": "/projects-static/climate-map/static/css/main.e29a5943.chunk.css"
+  },
+  {
+    "revision": "6a876f90c82bf32b3b1c",
+    "url": "/projects-static/climate-map/static/js/2.58891d61.chunk.js"
+  },
+  {
+    "revision": "ee34d0f2d1d06542f3f8",
+    "url": "/projects-static/climate-map/static/js/main.36030f40.chunk.js"
+  },
+  {
+    "revision": "7031de035a1f01fa67b5",
+    "url": "/projects-static/climate-map/static/js/runtime~main.ae5bf0e8.js"
+  }
+]);
